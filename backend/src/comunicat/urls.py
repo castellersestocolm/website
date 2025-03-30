4e49ef4d9bdfaadf97e2630fb90c957231f04165
Oriol Closa <oriol@5monkeys.se>
2025-03-30 20:12:17 +0200

    Media files

--- backend/src/comunicat/urls.py
+++ backend/src/comunicat/urls.py
@@ -1,6 +1,6 @@
-from django.conf.urls.static import static
+from django.views.static import serve
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 
 from comunicat import views
@@ -18,4 +18,6 @@ if "social_django" in settings.INSTALLED_APPS:
     ]
 
 # TODO: Temporary until they are served properly
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+]
