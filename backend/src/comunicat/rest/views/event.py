1bfab9f7840a7407806b6899a09fb3c7b9c8d579
Oriol Closa <oriol@5monkeys.se>
2025-04-12 22:26:43 +0200

    Increase event page size

--- backend/src/comunicat/rest/views/event.py
+++ backend/src/comunicat/rest/views/event.py
@@ -21,7 +21,7 @@ import event.api.registration
 
 
 class EventResultsSetPagination(PageNumberPagination):
-    page_size = 10
+    page_size = 20
     page_size_query_param = "page_size"
     max_page_size = 100
 
