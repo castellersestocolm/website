55f10c9ad603e11f21ef6213f336b7c18034297b
Oriol Closa <oriol@5monkeys.se>
2026-02-09 13:58:49 +0100

    Read-only

---
+++ backend/src/comunicat/utils/email.py
@@ -0,0 +1,20 @@
+from comunicat.consts import DOMAIN_BY_MODULE
+from comunicat.enums import Module
+from user.models import User
+
+
+def get_module_emails_from_user(user_obj: User, module: Module) -> list[str]:
+    email_domain = f"@{DOMAIN_BY_MODULE[module]}"
+    return [
+                         tmp_email
+                         for tmp_email in [user_obj.email]
+                                          + (
+                                              [
+                                                  user_email_obj.email
+                                                  for user_email_obj in user_obj.emails.all()
+                                              ]
+                                              if hasattr(user_obj, "emails")
+                                              else []
+                                          )
+                         if tmp_email.endswith(email_domain)
+                     ][:1]
