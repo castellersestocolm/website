a6777da913a9ea22ca91f164e76871ba19c0ab22
Oriol Closa <oriol@5monkeys.se>
2026-04-23 12:23:53 +0200

    Fix

--- backend/src/user/api/__init__.py
+++ backend/src/user/api/__init__.py
@@ -126,7 +126,6 @@ def get_list(
             with_pending=with_pending_membership,
             with_expired=with_expired_membership,
             modules=modules,
-            only_check=True,
         ).filter(has_active_membership=with_active_membership)
 
     if team_ids:
