779f5398f086596b31cf307a0e9e9cac353b52cd
Oriol Closa <oriol@5monkeys.se>
2026-05-24 22:46:11 +0200

    Store

--- frontend-org/app/src/components/FormNewsletter/FormNewsletter.tsx
+++ frontend-org/app/src/components/FormNewsletter/FormNewsletter.tsx
@@ -14,9 +14,8 @@ import { ConsentType } from "../../enums";
 import IconEast from "@mui/icons-material/East";
 import { LoaderClip } from "../LoaderClip/LoaderClip";
 import Box from "@mui/material/Box";
-import QRCode from "qrcode";
 import { useState } from "react";
-import IconMarkEmailReadOutlined from "@mui/icons-material/MarkEmailReadOutlined";
+import IconDoneOutlined from '@mui/icons-material/DoneOutlined';
 
 const FormGrid = styled(Grid)(() => ({
   display: "flex",
@@ -129,196 +128,200 @@ export default function FormNewsletter() {
 
   return newslettersIdsSelected !== undefined ? (
     submitted ? (
-        <Box className={styles.success}>
-          <IconMarkEmailReadOutlined className={styles.joinIcon} />
-          <Typography variant="h5" className={styles.joinSubtitle}>
-            {t("pages.about-newsletter.success")}
-          </Typography>
-        </Box>
-      ) : <form onSubmit={handleSubmit}>
-      <Grid container spacing={3}>
-        {!user && (
-          <>
-            <FormGrid size={{ xs: 12, md: 6 }}>
-              <FormLabel htmlFor="firstname" required>
-                {t("pages.user-join.form.first-name")}
-              </FormLabel>
-              <OutlinedInput
-                id="firstname"
-                name="firstname"
-                type="text"
-                placeholder="Namn"
-                autoComplete="first name"
-                required
-                size="small"
-                error={
-                  validationErrors &&
-                  validationErrors.firstname &&
-                  validationErrors.firstname[0].detail
-                }
-              />
-              {validationErrors && validationErrors.firstname && (
-                <FormHelperText error>
-                  {validationErrors.firstname[0].detail}
-                </FormHelperText>
-              )}
-            </FormGrid>
-            <FormGrid size={{ xs: 12, md: 6 }}>
-              <FormLabel htmlFor="lastname" required>
-                {t("pages.user-join.form.last-name")}
-              </FormLabel>
-              <OutlinedInput
-                id="lastname"
-                name="lastname"
-                type="text"
-                placeholder="Namnsson"
-                autoComplete="last name"
-                required
-                size="small"
-                error={
-                  validationErrors &&
-                  validationErrors.lastname &&
-                  validationErrors.lastname[0].detail
-                }
-              />
-              {validationErrors && validationErrors.lastname && (
-                <FormHelperText error>
-                  {validationErrors.lastname[0].detail}
-                </FormHelperText>
-              )}
-            </FormGrid>
-            <FormGrid size={{ xs: 12, md: 6 }}>
-              <FormLabel htmlFor="email" required>
-                {t("pages.user-join.form.email")}
-              </FormLabel>
-              <OutlinedInput
-                id="email"
-                name="email"
-                type="email"
-                placeholder="namn@namnsson.se"
-                autoComplete="email"
-                required
-                size="small"
-                error={
-                  validationErrors &&
-                  validationErrors.email &&
-                  validationErrors.email[0].detail
-                }
-              />
-              {validationErrors && validationErrors.email && (
-                <FormHelperText error>
-                  {validationErrors.email[0].detail}
-                </FormHelperText>
-              )}
-            </FormGrid>
-            <FormGrid size={{ xs: 12, md: 6 }}>
-              <FormLabel htmlFor="phone" required>
-                {t("pages.user-join.form.phone")}
-              </FormLabel>
-              <OutlinedInput
-                id="phone"
-                name="phone"
-                type="phone"
-                placeholder="+4687461000"
-                autoComplete="phone"
-                required
-                size="small"
-                error={
-                  validationErrors &&
-                  validationErrors.phone &&
-                  validationErrors.phone[0].detail
-                }
-              />
-              {validationErrors && validationErrors.phone && (
-                <FormHelperText error>
-                  {validationErrors.phone[0].detail}
-                </FormHelperText>
-              )}
-            </FormGrid>
-          </>
-        )}
-        {newsletters &&
-          newsletters.results &&
-          newsletters.results.length > 0 &&
-          newsletters.results.map((newsletter: any) => {
-            return (
-              <FormGrid size={{ xs: 12 }} key={newsletter.id}>
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      name="consent_pictures"
-                      value="yes"
-                      checked={
-                        newslettersIdsSelected &&
-                        newslettersIdsSelected.includes(newsletter.id)
-                      }
-                      onChange={(e) =>
-                        handleNewsletterCheckbox(e, newsletter.id)
-                      }
-                    />
-                  }
-                  label={
-                    <>
-                      <Typography variant="body1">{newsletter.name}</Typography>
-                      {newsletter.description && (
-                        <Typography variant="body2">
-                          {newsletter.description}
-                        </Typography>
-                      )}
-                    </>
+      <Box className={styles.success}>
+        <IconDoneOutlined className={styles.joinIcon} />
+        <Typography variant="h5" className={styles.joinSubtitle}>
+          {t("pages.about-newsletter.success")}
+        </Typography>
+      </Box>
+    ) : (
+      <form onSubmit={handleSubmit}>
+        <Grid container spacing={3}>
+          {!user && (
+            <>
+              <FormGrid size={{ xs: 12, md: 6 }}>
+                <FormLabel htmlFor="firstname" required>
+                  {t("pages.user-join.form.first-name")}
+                </FormLabel>
+                <OutlinedInput
+                  id="firstname"
+                  name="firstname"
+                  type="text"
+                  placeholder="Namn"
+                  autoComplete="first name"
+                  required
+                  size="small"
+                  error={
+                    validationErrors &&
+                    validationErrors.firstname &&
+                    validationErrors.firstname[0].detail
                   }
                 />
+                {validationErrors && validationErrors.firstname && (
+                  <FormHelperText error>
+                    {validationErrors.firstname[0].detail}
+                  </FormHelperText>
+                )}
               </FormGrid>
-            );
-          })}
-        <FormGrid size={{ xs: 12 }}>
-          <FormControlLabel
-            control={
-              <Checkbox
-                name="confirmCasal"
-                value="yes"
-                checked={consentGeneralChecked}
-                onChange={(e) => setConsentGeneralChecked(e.target.checked)}
-              />
-            }
-            label={t("pages.user-join.form.checkbox-processing")}
-            required={true}
-          />
-          <Typography>
-            <Link
-              href={t("pages.user-join.form.checkbox-processing-link.href")}
-              color="secondary"
-              underline="none"
-              target="_blank"
-              className={styles.checkboxLink}
-            >
-              {t("pages.user-join.form.checkbox-processing-link.title")}
-              <IconEast className={styles.iconEast} />
-            </Link>
-          </Typography>
-        </FormGrid>
-        <FormGrid size={{ xs: 12 }}>
-          <Stack direction="row" spacing={2} className={styles.buttons}>
-            <Button
-              variant="contained"
-              type="submit"
-              name="join-backend"
-              disableElevation
-            >
-              {t("pages.about-newsletter.form.button-complete")}
-            </Button>
-          </Stack>
-        </FormGrid>
-        {validationErrors &&
-          (validationErrors.throttle || validationErrors.error) && (
-            <FormGrid size={{ xs: 12 }}>
-              <FormHelperText error className={styles.error}>
-                {validationErrors.throttle || validationErrors.error}
-              </FormHelperText>
-            </FormGrid>
+              <FormGrid size={{ xs: 12, md: 6 }}>
+                <FormLabel htmlFor="lastname" required>
+                  {t("pages.user-join.form.last-name")}
+                </FormLabel>
+                <OutlinedInput
+                  id="lastname"
+                  name="lastname"
+                  type="text"
+                  placeholder="Namnsson"
+                  autoComplete="last name"
+                  required
+                  size="small"
+                  error={
+                    validationErrors &&
+                    validationErrors.lastname &&
+                    validationErrors.lastname[0].detail
+                  }
+                />
+                {validationErrors && validationErrors.lastname && (
+                  <FormHelperText error>
+                    {validationErrors.lastname[0].detail}
+                  </FormHelperText>
+                )}
+              </FormGrid>
+              <FormGrid size={{ xs: 12, md: 6 }}>
+                <FormLabel htmlFor="email" required>
+                  {t("pages.user-join.form.email")}
+                </FormLabel>
+                <OutlinedInput
+                  id="email"
+                  name="email"
+                  type="email"
+                  placeholder="namn@namnsson.se"
+                  autoComplete="email"
+                  required
+                  size="small"
+                  error={
+                    validationErrors &&
+                    validationErrors.email &&
+                    validationErrors.email[0].detail
+                  }
+                />
+                {validationErrors && validationErrors.email && (
+                  <FormHelperText error>
+                    {validationErrors.email[0].detail}
+                  </FormHelperText>
+                )}
+              </FormGrid>
+              <FormGrid size={{ xs: 12, md: 6 }}>
+                <FormLabel htmlFor="phone" required>
+                  {t("pages.user-join.form.phone")}
+                </FormLabel>
+                <OutlinedInput
+                  id="phone"
+                  name="phone"
+                  type="phone"
+                  placeholder="+4687461000"
+                  autoComplete="phone"
+                  required
+                  size="small"
+                  error={
+                    validationErrors &&
+                    validationErrors.phone &&
+                    validationErrors.phone[0].detail
+                  }
+                />
+                {validationErrors && validationErrors.phone && (
+                  <FormHelperText error>
+                    {validationErrors.phone[0].detail}
+                  </FormHelperText>
+                )}
+              </FormGrid>
+            </>
           )}
-      </Grid>
-    </form>
+          {newsletters &&
+            newsletters.results &&
+            newsletters.results.length > 0 &&
+            newsletters.results.map((newsletter: any) => {
+              return (
+                <FormGrid size={{ xs: 12 }} key={newsletter.id}>
+                  <FormControlLabel
+                    control={
+                      <Checkbox
+                        name="consent_pictures"
+                        value="yes"
+                        checked={
+                          newslettersIdsSelected &&
+                          newslettersIdsSelected.includes(newsletter.id)
+                        }
+                        onChange={(e) =>
+                          handleNewsletterCheckbox(e, newsletter.id)
+                        }
+                      />
+                    }
+                    label={
+                      <>
+                        <Typography variant="body1">
+                          {newsletter.name}
+                        </Typography>
+                        {newsletter.description && (
+                          <Typography variant="body2">
+                            {newsletter.description}
+                          </Typography>
+                        )}
+                      </>
+                    }
+                  />
+                </FormGrid>
+              );
+            })}
+          <FormGrid size={{ xs: 12 }}>
+            <FormControlLabel
+              control={
+                <Checkbox
+                  name="confirmCasal"
+                  value="yes"
+                  checked={consentGeneralChecked}
+                  onChange={(e) => setConsentGeneralChecked(e.target.checked)}
+                />
+              }
+              label={t("pages.user-join.form.checkbox-processing")}
+              required={true}
+            />
+            <Typography>
+              <Link
+                href={t("pages.user-join.form.checkbox-processing-link.href")}
+                color="secondary"
+                underline="none"
+                target="_blank"
+                className={styles.checkboxLink}
+              >
+                {t("pages.user-join.form.checkbox-processing-link.title")}
+                <IconEast className={styles.iconEast} />
+              </Link>
+            </Typography>
+          </FormGrid>
+          <FormGrid size={{ xs: 12 }}>
+            <Stack direction="row" spacing={2} className={styles.buttons}>
+              <Button
+                variant="contained"
+                type="submit"
+                name="join-backend"
+                disableElevation
+              >
+                {t("pages.about-newsletter.form.button-complete")}
+              </Button>
+            </Stack>
+          </FormGrid>
+          {validationErrors &&
+            (validationErrors.throttle || validationErrors.error) && (
+              <FormGrid size={{ xs: 12 }}>
+                <FormHelperText error className={styles.error}>
+                  {validationErrors.throttle || validationErrors.error}
+                </FormHelperText>
+              </FormGrid>
+            )}
+        </Grid>
+      </form>
+    )
   ) : (
     <Box className={styles.pageLoader}>
       <LoaderClip />
