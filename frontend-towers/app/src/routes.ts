5901dfc298830e193b370c1056cb04683daa8ade
Oriol Closa <oriol@5monkeys.se>
2026-08-06 12:55:28 +0200

    Add policy on footer

--- frontend-towers/app/src/routes.ts
+++ frontend-towers/app/src/routes.ts
@@ -136,6 +136,9 @@ export const ROUTES = {
   "external-casal-form-membership": {
     path: "https://lesquatrebarres.org/user/join",
   },
+  "external-casal-policy-privacy": {
+    path: "https://lesquatrebarres.org/policy/privacy",
+  },
   "external-berlin-diada-2025": {
     path: "https://castellers.berlin/international-diada-castellera-berlin-2025/",
   },
