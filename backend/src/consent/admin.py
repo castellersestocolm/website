2ca9b3964f0bdc13ad36d4ce5427cc07cc4a1fa5
Oriol Closa <oriol@5monkeys.se>
2026-08-27 10:50:03 +0200

    Lint

--- backend/src/consent/admin.py
+++ backend/src/consent/admin.py
@@ -11,7 +11,14 @@ class EntityConsentAdmin(admin.ModelAdmin):
         "entity__lastname",
         "entity__email",
     )
-    list_display = ("entity", "module", "type", "newsletter", "deleted_at", "created_at")
+    list_display = (
+        "entity",
+        "module",
+        "type",
+        "newsletter",
+        "deleted_at",
+        "created_at",
+    )
     list_filter = ("module", "type", "newsletter")
     raw_id_fields = ("entity",)
     readonly_fields = ("google_group_user",)
