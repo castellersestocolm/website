d1923fa02e8d146ad307af40f8e1fa394078067e
Oriol Closa <oriol@5monkeys.se>
2026-09-01 10:49:06 +0200

    Payment description changes

--- backend/src/order/api/tests/test_order.py
+++ backend/src/order/api/tests/test_order.py
@@ -595,7 +595,7 @@ class TestComplete(NumOperationsMixin, TestCase):
             ),
         ):
             with self.assertNumOperations(
-                num=0, num_selects=58, num_inserts=9, num_updates=6
+                num=0, num_selects=53, num_inserts=9, num_updates=6
             ):
                 order_obj = complete(
                     order_id=self.order_1_obj.id,
