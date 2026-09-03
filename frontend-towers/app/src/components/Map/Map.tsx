c84f73058091956e4785b7e435a44ec3eb66eaf9
Oriol Closa <oriol@5monkeys.se>
2026-09-03 14:03:12 +0200

    Fix CARTO map key layer

--- frontend-towers/app/src/components/Map/Map.tsx
+++ frontend-towers/app/src/components/Map/Map.tsx
@@ -39,7 +39,7 @@ export default function Map({
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-        url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+        url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png?key=cb1_2up7_1_d0d414edadc82b3cc1eaec4f"
       />
       <MapMarker
         name={location ? location.name : undefined}
