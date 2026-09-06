c2078734f915e2f2e4f91fe9b24763d180a69516
Oriol Closa <10714286+oriolclosa@users.noreply.github.com>
2026-09-06 21:35:13 +0200

    Dashboard membership status fix (#117)
    * Fix action quotes
    
    * JSON as base 64
    
    * Dashboard membership status fix
    
    ---------
    
    Co-authored-by: Oriol Closa <oriol@5monkeys.se>

--- frontend-towers/app/src/pages/user-dashboard/index.tsx
+++ frontend-towers/app/src/pages/user-dashboard/index.tsx
@@ -804,7 +804,7 @@ function UserDashboardPage() {
           )}
         </Box>
         {user &&
-          user.permission_level >= PermissionLevel.ADMIN &&
+          user.permission_level >= PermissionLevel.ADMIN && membership &&
           membership.status === MembershipStatus.ACTIVE && <WalletGoogle />}
         <Divider />
         {programCourses && (
