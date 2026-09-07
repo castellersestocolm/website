ee1553724fd082045e79aeac3114eebed6d35d7a
Oriol Closa <oriol@5monkeys.se>
2026-09-07 11:55:16 +0200

    Add event Google Wallet pass

--- backend/src/comunicat/rest/serializers/integration.py
+++ backend/src/comunicat/rest/serializers/integration.py
@@ -3,3 +3,7 @@ from rest_framework import serializers as s
 
 class IntegrationGoogleWalletPassLoyaltySerializer(s.Serializer):
     url = s.CharField(read_only=True)
+
+
+class IntegrationGoogleWalletPassEventSerializer(s.Serializer):
+    url = s.CharField(read_only=True)
--- backend/src/integration/api/google/wallet.py
+++ backend/src/integration/api/google/wallet.py
@@ -4,7 +4,7 @@ from uuid import UUID
 
 from django.conf import settings
 from django.db.models import Prefetch
-from django.utils import timezone
+from django.utils import timezone, translation
 from google.auth import crypt, jwt
 from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import Resource, build
@@ -17,7 +17,7 @@ from comunicat.consts import (
 )
 from comunicat.enums import Module
 from comunicat.template_tags.comunicat_tags import full_url
-from integration.consts import GOOGLE_WALLET_SCOPES
+from integration.consts import GOOGLE_WALLET_SCOPES, LANGUAGE_TO_GOOGLE_LANGUAGE
 from membership.enums import MembershipStatus
 
 _log = logging.getLogger(__name__)
@@ -92,7 +92,6 @@ class GoogleWalletLoyalty:
 
     # https://developers.google.com/wallet/retail/loyalty-cards/rest/v1/loyaltyclass
     def get_class(self) -> dict:
-
         name = NAME_BY_MODULE[self.module]
         file_logo = full_url(
             path=FILE_LOGO_BY_MODULE[self.module],
@@ -167,7 +166,6 @@ class GoogleWalletLoyalty:
         return new_class
 
     def get_object(self) -> dict | None:
-
         new_object = {
             "id": f"{settings.INTEGRATION_GOOGLE_WALLET_ISSUER_ID}.loyalty.{Module(self.module).name.lower()}.{self.user_obj.membership_number}",
             "classId": f"{settings.INTEGRATION_GOOGLE_WALLET_ISSUER_ID}.loyalty.{Module(self.module).name.lower()}",
@@ -179,29 +177,159 @@ class GoogleWalletLoyalty:
                 "start": {"date": self.membership_start},
                 "end": {"date": self.membership_end},
             },
-            # "messages": [
-            #     {
-            #         "id": "modules",
-            #         "localizedHeader": {
-            #             "defaultValue": {"language": "ca", "value": "Membre"},
-            #             "translatedValues": [
-            #                 {"language": "en-GB", "value": "Member"},
-            #                 {"language": "sv", "value": "Medlem"},
-            #             ],
-            #         },
-            #         "body": ", ".join(
-            #             [
-            #                 NAME_BY_MODULE[membership_module_obj.module]
-            #                 for membership_module_obj in self.membership_obj.all_modules
-            #             ]
-            #         ),
-            #         "displayInterval": {
-            #             "start": {"date": self.membership_start},
-            #             "end": {"date": self.membership_end},
-            #         },
-            #         "messageType": "TEXT",
-            #     }
-            # ],
+        }
+
+        return new_object
+
+    def get_url(self) -> str:
+        new_class = self.get_class()
+        new_object = self.get_object()
+
+        origin_url = full_url(module=self.module)
+
+        claims = {
+            "iss": settings.INTEGRATION_GOOGLE_EMAIL,
+            "aud": "google",
+            "origins": [origin_url],
+            "typ": "savetowallet",
+            "payload": {
+                "loyaltyClasses": [new_class],
+                "loyaltyObjects": [new_object],
+            },
+        }
+
+        signer = crypt.RSASigner.from_service_account_file(self.key_file_path)
+        token = jwt.encode(signer, claims).decode("utf-8")
+
+        return f"https://pay.google.com/gp/v/save/{token}"
+
+
+# https://github.com/google-wallet/rest-samples/blob/main/python/demo_loyalty.py
+class GoogleWalletEvent:
+    key_file_path: str
+    credentials: Credentials
+    client: Resource
+
+    def __init__(self, registration_id: UUID):
+        self.key_file_path = f"{settings.INTEGRATION_GOOGLE_CRED_DIR}app.json"
+        self.auth()
+
+        from event.models import Event, Registration
+
+        self.registration_obj = (
+            Registration.objects.filter(id=registration_id)
+            .select_related(
+                "event", "event__location", "entity", "entity__user", "price"
+            )
+            .first()
+        )
+
+        if not self.registration_obj:
+            return
+
+        self.event_obj = self.registration_obj.event
+
+        self.event_start = self.event_obj.time_from.isoformat()
+        self.event_end = self.event_obj.time_to.isoformat()
+
+        self.registration_key = str(self.registration_obj.id).replace("-", "").lower()
+        self.event_key = str(self.event_obj.id).replace("-", "").lower()
+
+        self.module = self.registration_obj.event.module
+
+    def auth(self):
+        self.credentials = Credentials.from_service_account_file(
+            self.key_file_path, scopes=GOOGLE_WALLET_SCOPES
+        )
+
+        self.client = build("walletobjects", "v1", credentials=self.credentials)
+
+    # https://developers.google.com/wallet/reference/rest/v1/eventticketclass
+    def get_class(self) -> dict:
+        name = NAME_BY_MODULE[self.module]
+        locale = translation.get_language()
+        palette_colour = PALETTE_BY_MODULE[self.module]["primary"]["light"]
+
+        new_class = {
+            "id": f"{settings.INTEGRATION_GOOGLE_WALLET_ISSUER_ID}.event.{self.event_key}",
+            "eventId": f"{settings.INTEGRATION_GOOGLE_WALLET_ISSUER_ID}.event.{self.event_key}",
+            "eventName": {
+                "defaultValue": {
+                    "language": LANGUAGE_TO_GOOGLE_LANGUAGE.get(
+                        locale, LANGUAGE_TO_GOOGLE_LANGUAGE[settings.LANGUAGE_CODE]
+                    ),
+                    "value": self.event_obj.title_locale,
+                },
+                "translatedValues": [
+                    {
+                        "language": LANGUAGE_TO_GOOGLE_LANGUAGE[current_locale],
+                        "value": current_title,
+                    }
+                    for current_locale, current_title in self.event_obj.title.items()
+                    if current_locale != locale
+                    and current_locale in LANGUAGE_TO_GOOGLE_LANGUAGE
+                ],
+            },
+            "issuerName": name,
+            "reviewStatus": "UNDER_REVIEW",
+            "multipleDevicesAndHoldersAllowedStatus": "ONE_USER_ALL_DEVICES",
+            "hexBackgroundColor": palette_colour,
+            "dateTime": {
+                "start": {"date": self.event_start},
+                "end": {"date": self.event_end},
+            },
+            **(
+                {
+                    "merchantLocations": [
+                        {
+                            "latitude": self.event_obj.location.coordinate_lat,
+                            "longitude": self.event_obj.location.coordinate_lon,
+                        }
+                    ]
+                }
+                if self.event_obj.location
+                else {}
+            ),
+            **(
+                {"heroImage": self.event_obj.picture.url}
+                if self.event_obj.picture
+                else {}
+            ),
+        }
+
+        return new_class
+
+    def get_object(self) -> dict | None:
+        new_object = {
+            "id": f"{settings.INTEGRATION_GOOGLE_WALLET_ISSUER_ID}.registration.{self.registration_key}",
+            "classId": f"{settings.INTEGRATION_GOOGLE_WALLET_ISSUER_ID}.event.{self.event_key}",
+            "state": "ACTIVE" if self.registration_obj.is_active else "INACTIVE",
+            "barcode": {
+                "type": "QR_CODE",
+                "value": str(self.registration_obj.id),
+                "alternateText": self.registration_obj.entity.name,
+            },
+            "ticketHolderName": self.registration_obj.entity.name,
+            # ticketType
+            # groupingInfo
+            **(
+                {
+                    "faceValue": {
+                        "micros": int(
+                            1000000 * self.registration_obj.price.amount.amount
+                        ),
+                        "currencyCode": str(
+                            self.registration_obj.price.amount.currency
+                        ),
+                    }
+                }
+                if self.registration_obj.price
+                else {}
+            ),
+            "validTimeInterval": {
+                "end": {"date": self.event_end},
+            },
+            # messages
         }
 
         return new_object
@@ -237,3 +365,13 @@ def get_pass_loyalty_url(user_id: UUID, module: Module) -> str | None:
         return None
 
     return google_wallet_loyalty.get_url()
+
+
+def get_pass_event_url(registration_id: UUID) -> str | None:
+    try:
+        google_wallet_event = GoogleWalletEvent(registration_id=registration_id)
+    except Exception as e:
+        _log.exception(e)
+        return None
+
+    return google_wallet_event.get_url()
