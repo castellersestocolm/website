1903c2fe9a8461f4a2285569303a136631b8e1ca
Oriol Closa <oriol@5monkeys.se>
2026-09-07 18:54:54 +0200

    Localise Google Wallet on frontend too

--- frontend-towers/app/src/components/WalletGoogle/WalletGoogle.tsx
+++ frontend-towers/app/src/components/WalletGoogle/WalletGoogle.tsx
@@ -2,10 +2,13 @@ import * as React from "react";
 import styles from "./styles.module.css";
 import { useAppContext } from "../AppContext/AppContext";
 import { apiIntegrationGoogleWalletPassLoyaltyGet } from "../../api";
-import GoogleWalletImage from "../../assets/images/google/wallet/wallet.png";
 import { Box, Link } from "@mui/material";
+import { useTranslation } from "react-i18next";
+import { LANGUAGE_TO_GOOGLE_LOCALE_COUNTRY } from "../../consts";
 
 export default function WalletGoogle() {
+  const { i18n } = useTranslation("common");
+
   const { user } = useAppContext();
 
   const [googleWalletPassLoyalty, setGoogleWalletPassLoyalty] =
@@ -21,12 +24,19 @@ export default function WalletGoogle() {
     }
   }, [user, setGoogleWalletPassLoyalty]);
 
+  const googleLocaleCountry = (LANGUAGE_TO_GOOGLE_LOCALE_COUNTRY as any)[
+    i18n.resolvedLanguage
+  ];
+  const backgroundImage = require(
+    `../../assets/images/integration/google/wallet/png/${googleLocaleCountry}_add_to_google_wallet_add-wallet-badge.png`,
+  );
+
   return (
     <>
       {googleWalletPassLoyalty && googleWalletPassLoyalty.url && (
         <Box className={styles.walletBox}>
           <Link href={googleWalletPassLoyalty.url}>
-            <img src={GoogleWalletImage} />
+            <img src={backgroundImage} alt="Google Wallet" />
           </Link>
         </Box>
       )}
