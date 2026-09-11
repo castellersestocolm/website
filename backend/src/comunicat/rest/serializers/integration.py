d4e163d1174a070bca68c1809dedd0d6e0f55d52
Oriol Closa <10714286+oriolclosa@users.noreply.github.com>
2026-09-11 21:03:34 +0200

    Minor membership fixes  (#141)
    * Minor membership fixes
    
    * Minor membership fixes
    
    * Minor membership fixes
    
    ---------
    
    Co-authored-by: Oriol Closa <oriol@5monkeys.se>

--- backend/src/comunicat/rest/serializers/integration.py
+++ backend/src/comunicat/rest/serializers/integration.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers as s
 
+from comunicat.enums import Module
+from comunicat.rest.utils.fields import IntEnumField
+
 
 class IntegrationGoogleWalletPassLoyaltySerializer(s.Serializer):
     url = s.CharField(read_only=True)
@@ -11,3 +14,4 @@ class IntegrationGoogleWalletPassEventSerializer(s.Serializer):
 
 class IntegrationAppleWalletPassLoyaltyRequestSerializer(s.Serializer):
     token = s.CharField(required=False)
+    module = IntEnumField(Module, required=False)
